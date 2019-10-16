04c77ae2a19049c5a77f651be222f33e1d326cdf
Dan Holtby <djholtby@gmail.com>
2019-10-16 14:45:38 -0400

    add build-deps

--- info.rkt
+++ info.rkt
@@ -2,3 +2,6 @@
 (define collection "versioned-box")
 (define scribblings '(("scribblings/versioned-box.scrbl")))
 (define deps '("base"))
+(define build-deps '("racket-doc"
+                     "rackunit-lib"
+                     "scribble-lib"))
