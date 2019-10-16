f54d0d32e9497d7080a1553d73a5d4eb796c56a2
Dan Holtby <djholtby@gmail.com>
2019-10-16 14:54:20 -0400

    updated info.rkt

--- info.rkt
+++ info.rkt
@@ -1,5 +1,9 @@
 #lang info
+
 (define collection "versioned-box")
+(define pkg-name "versioned-box")
+(define pkg-desc "MVCC using versioned boxes")
+(define pkg-authors '(djholtby))
 (define scribblings '(("scribblings/versioned-box.scrbl")))
 (define deps '("base"))
 (define build-deps '("racket-doc"
